722711de3c40b4e7f6ef21b7e6dc1752ab57b2a9
Luca <55286357+luclanz@users.noreply.github.com>
2021-08-10 22:02:18 +0200

    RTC update
    Added functions for computing the time remaining for the alarm

--- Study/DS3231_Study/DS3231_Study.ino
+++ Study/DS3231_Study/DS3231_Study.ino
@@ -11,35 +11,39 @@ void setup() {
 
     rtc_setup(CLOCK_INTERRUPT_PIN);
 
-    set_timer_alarm (10);
+    rtc_set_timer_alarm (10);
+
+    Serial.print(rtc_hours_to_alarm(21, 12, 18 < 49));
+    Serial.print(":");
+    Serial.println(rtc_minutes_to_alarm(49, 18));
 
 }
 
 void loop() {
-    // print current time
-    char date[10] = "hh:mm:ss";
-    rtc.now().toString(date);
-    Serial.print(date);
-    // the value at SQW-Pin (because of pullup 1 means no alarm)
-    Serial.print(" SQW: ");
-    Serial.print(digitalRead(CLOCK_INTERRUPT_PIN));
-    // whether a alarm happened happened
-    Serial.print(" Alarm1: ");
-    Serial.print(rtc.alarmFired(1));
-    Serial.print(" Alarm2: ");
-    Serial.println(rtc.alarmFired(2));
-    // control register values (see https://datasheets.maximintegrated.com/en/ds/DS3231.pdf page 13)
-    // Serial.print(" Control: 0b");
-    // Serial.println(read_i2c_register(DS3231_ADDRESS, DS3231_CONTROL), BIN);
-    
-    // resetting SQW and alarm 1 flag
-    // using setAlarm1, the next alarm could now be configurated
-    if(rtc.alarmFired(1)) {
-        rtc.clearAlarm(1);
-        Serial.println("Alarm cleared");
-    }
-    
-    delay(2000);
+//    // print current time
+//    char date[10] = "hh:mm:ss";
+//    rtc.now().toString(date);
+//    Serial.print(date);
+//    // the value at SQW-Pin (because of pullup 1 means no alarm)
+//    Serial.print(" SQW: ");
+//    Serial.print(digitalRead(CLOCK_INTERRUPT_PIN));
+//    // whether a alarm happened happened
+//    Serial.print(" Alarm1: ");
+//    Serial.print(rtc.alarmFired(1));
+//    Serial.print(" Alarm2: ");
+//    Serial.println(rtc.alarmFired(2));
+//    // control register values (see https://datasheets.maximintegrated.com/en/ds/DS3231.pdf page 13)
+//    // Serial.print(" Control: 0b");
+//    // Serial.println(read_i2c_register(DS3231_ADDRESS, DS3231_CONTROL), BIN);
+//    
+//    // resetting SQW and alarm 1 flag
+//    // using setAlarm1, the next alarm could now be configurated
+//    if(rtc.alarmFired(1)) {
+//        rtc.clearAlarm(1);
+//        Serial.println("Alarm cleared");
+//    }
+//    
+//    delay(2000);
 }
 
 void onAlarm() {
--- Study/DS3231_Study/rtc_func.ino
+++ Study/DS3231_Study/rtc_func.ino
@@ -36,17 +36,35 @@ void rtc_setup(int interrupt) //int: interrupt pin for the ds3231
 }
 
 
-void set_timer_alarm (int sec_to_alarm) //int: second of the timer 
+void rtc_set_timer_alarm (int sec_to_alarm) //int: second of the timer 
 {
   
       // schedule an alarm sec_to_alarm seconds in the future --> NOTE: FOR THE TIMER WE SET THE SECOND ALARM
     if(!rtc.setAlarm1(
-            rtc.now() + TimeSpan(sec_to_alarm),
-            DS3231_A1_Second // this mode triggers the alarm when the seconds match. See Doxygen for other options
+            rtc.now() + TimeSpan(0,0,0,sec_to_alarm),     //with this it will set a timespan of (dd, hh, mm, ss).
+            DS3231_A1_Second                              // this mode triggers the alarm when the seconds match. See Doxygen for other options
     )) {
         Serial.println("Error, alarm wasn't set!");
     }else {
         Serial.print("Alarm will happen in ");
         Serial.println(sec_to_alarm);  
     }
+}
+
+int rtc_minutes_to_alarm (int m, int m_alarm) //first: actual time, second: desired alarm time
+{
+  if (m_alarm < m) {
+    return (m_alarm + 60 - m);
+  }
+
+  return (m_alarm - m);
+}
+
+int rtc_hours_to_alarm (int h, int h_alarm, bool carrier) //first: actual time, second: desired alarm time, third: (m_alarm < m)
+{
+  if (h_alarm < h) {
+    return (h_alarm + 24 - h - 1 * carrier); 
+  }
+
+  return (h_alarm - h - 1 * carrier);
 }
